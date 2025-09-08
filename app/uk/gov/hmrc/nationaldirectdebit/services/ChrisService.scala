6dad073c0a53f800ead5a2f339ad9a847ef6107d
javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2025-09-08 16:25:37 +0100

    DTR-121 add known facts

--- app/uk/gov/hmrc/nationaldirectdebit/services/ChrisService.scala
+++ app/uk/gov/hmrc/nationaldirectdebit/services/ChrisService.scala
@@ -7,9 +7,10 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing,
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -36,12 +37,43 @@ class ChrisService @Inject()(chrisConnector: ChrisConnector)(implicit ec: Execut
     chrisConnector.submitEnvelope(envelopeXml)
   }
 
+  // ============================
+  // Lookups for HOD service & known facts
+  // ============================
+  private val serviceToHod: Map[DirectDebitSource, String] = Map(
+    DirectDebitSource.CT   -> "COTA",
+    DirectDebitSource.PAYE -> "PAYE",
+    DirectDebitSource.SA   -> "CESA",
+    DirectDebitSource.TC   -> "NTC",
+    DirectDebitSource.VAT  -> "VAT",
+    DirectDebitSource.MGD  -> "MGD",
+    DirectDebitSource.NIC  -> "NIDN",
+    DirectDebitSource.OL   -> "SAFE",
+    DirectDebitSource.SDLT -> "SDLT"
+  )
+
+  private case class KnownFactConfig(factName: String, factType: String)
+
+  private val hodToKnownFact: Map[String, KnownFactConfig] = Map(
+    "COTA" -> KnownFactConfig("UTR", "CTUTR"),
+    "PAYE" -> KnownFactConfig("employerReference", "EMPREF"),
+    "CESA" -> KnownFactConfig("UTR", "UTR"),
+    "NTC"  -> KnownFactConfig("NINO", "NINO"),
+    "MGD"  -> KnownFactConfig("HMRCMGDRN", "MGDRN"),
+    "TPSS" -> KnownFactConfig("PSAID", "PSAID"),
+    "CIS"  -> KnownFactConfig("employerReference", "EMPREF")
+  )
+
   private def buildEnvelopeXml(request: ChrisSubmissionRequest, credId: String, affinityGroup: String): Elem = {
     val correlatingId = UUID.randomUUID().toString.replace("-", "")
     val receiptDate = LocalDateTime.now(ZoneOffset.UTC).format(dateTimeFormatter)
     val submissionDateTime = LocalDateTime.now(ZoneOffset.UTC).format(dateTimeFormatter)
     val periodEnd = calculatePeriodEnd()
     val senderType = if (affinityGroup == "agent") "Agent" else "Individual"
+
+    val hodService = serviceToHod(request.serviceType)
+    val knownFactCfg = hodToKnownFact.getOrElse(hodService, KnownFactConfig("UNKNOWN", "UNKNOWN"))
+    val knownFactValue = request.paymentReference.getOrElse("")
 
     <ChRISEnvelope xmlns="http://www.hmrc.gov.uk/ChRIS/Envelope/2">
       <EnvelopeVersion>2.0</EnvelopeVersion>
@@ -56,10 +88,10 @@ class ChrisService @Inject()(chrisConnector: ChrisConnector)(implicit ec: Execut
         </Sender>
       </Header>
       <Body>
-        <IRenvelope xmlns=" add ask for value">
+        <IRenvelope xmlns="add ask for value">
           <IRheader>
             <Keys>
-              <!-- TODO: populate known facts as <Key Type="...">value</Key> -->
+              <Key Type={knownFactCfg.factType}>{knownFactValue}</Key>
             </Keys>
             <PeriodEnd>{periodEnd}</PeriodEnd>
             <Sender>{senderType}</Sender>
@@ -67,7 +99,10 @@ class ChrisService @Inject()(chrisConnector: ChrisConnector)(implicit ec: Execut
           <dDIPPDetails>
             <submissionDateTime>{submissionDateTime}</submissionDateTime>
             <credentialID>{credId}</credentialID>
-            <!-- TODO: populate knownFact elements here -->
+            <knownFact>
+              <service>{hodService}</service>
+              <value>{knownFactValue}</value>
+            </knownFact>
             <directDebitInstruction>
               <actionType>01</actionType>
               <ddiReferenceNo>{request.ddiReferenceNo}</ddiReferenceNo>
@@ -76,7 +111,7 @@ class ChrisService @Inject()(chrisConnector: ChrisConnector)(implicit ec: Execut
               <bankAccountName>{request.bankName}</bankAccountName>
               {if (request.yourBankDetailsWithAuddisStatus.auddisStatus) <paperAuddisFlag>01</paperAuddisFlag> else scala.xml.Null}
             </directDebitInstruction>
-            {buildPaymentPlanXml(request)}
+            {buildPaymentPlanXml(request, hodService)}
           </dDIPPDetails>
         </IRenvelope>
       </Body>
@@ -91,66 +126,67 @@ class ChrisService @Inject()(chrisConnector: ChrisConnector)(implicit ec: Execut
     periodDate.toString
   }
 
-  private def buildPaymentPlanXml(request: ChrisSubmissionRequest): Elem = request.serviceType match {
-    case DirectDebitSource.CT =>
-      <paymentPlan>
-        <actionType>01</actionType>
-        <pPType>01</pPType>
-        <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
-        <hodService>CT</hodService>
-        <paymentCurrency>GBP</paymentCurrency>
-        <scheduledPaymentAmount>{request.paymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
-        <scheduledPaymentStartDate>{request.paymentDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
-        <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
-      </paymentPlan>
+  private def buildPaymentPlanXml(request: ChrisSubmissionRequest, hodService: String): Elem =
+    request.serviceType match {
+      case DirectDebitSource.CT =>
+        <paymentPlan>
+          <actionType>01</actionType>
+          <pPType>01</pPType>
+          <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
+          <hodService>{hodService}</hodService>
+          <paymentCurrency>GBP</paymentCurrency>
+          <scheduledPaymentAmount>{request.paymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
+          <scheduledPaymentStartDate>{request.paymentDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
+          <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
+        </paymentPlan>
 
-    case DirectDebitSource.TC =>
-      <paymentPlan>
-        <actionType>01</actionType>
-        <pPType>03</pPType>
-        <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
-        <hodService>TC</hodService>
-        <paymentCurrency>GBP</paymentCurrency>
-        <scheduledPaymentAmount>{request.regularPaymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
-        <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
-        <scheduledPaymentEndDate>{request.planEndDate.getOrElse("")}</scheduledPaymentEndDate>
-        <scheduledPaymentFrequency>05</scheduledPaymentFrequency>
-        <balancingPaymentAmount>{request.totalAmountDue.getOrElse(BigDecimal(0)) - request.regularPaymentAmount.getOrElse(BigDecimal(0))}</balancingPaymentAmount>
-        <balancingPaymentDate>{request.planEndDate.getOrElse("")}</balancingPaymentDate>
-        <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
-      </paymentPlan>
+      case DirectDebitSource.TC =>
+        <paymentPlan>
+          <actionType>01</actionType>
+          <pPType>03</pPType>
+          <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
+          <hodService>{hodService}</hodService>
+          <paymentCurrency>GBP</paymentCurrency>
+          <scheduledPaymentAmount>{request.regularPaymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
+          <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
+          <scheduledPaymentEndDate>{request.planEndDate.getOrElse("")}</scheduledPaymentEndDate>
+          <scheduledPaymentFrequency>05</scheduledPaymentFrequency>
+          <balancingPaymentAmount>{request.totalAmountDue.getOrElse(BigDecimal(0)) - request.regularPaymentAmount.getOrElse(BigDecimal(0))}</balancingPaymentAmount>
+          <balancingPaymentDate>{request.planEndDate.getOrElse("")}</balancingPaymentDate>
+          <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
+        </paymentPlan>
 
-    case DirectDebitSource.MGD =>
-      <paymentPlan>
-        <actionType>01</actionType>
-        <pPType>04</pPType>
-        <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
-        <hodService>MGD</hodService>
-        <paymentCurrency>GBP</paymentCurrency>
-        <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
-      </paymentPlan>
+      case DirectDebitSource.MGD =>
+        <paymentPlan>
+          <actionType>01</actionType>
+          <pPType>04</pPType>
+          <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
+          <hodService>{hodService}</hodService>
+          <paymentCurrency>GBP</paymentCurrency>
+          <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
+        </paymentPlan>
 
-    case DirectDebitSource.SA =>
-      <paymentPlan>
-        <actionType>01</actionType>
-        <pPType>02</pPType>
-        <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
-        <hodService>SA</hodService>
-        <paymentCurrency>GBP</paymentCurrency>
-        <scheduledPaymentAmount>{request.regularPaymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
-        <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
-        <scheduledPaymentEndDate>{request.planEndDate.getOrElse("")}</scheduledPaymentEndDate>
-        <scheduledPaymentFrequency>05</scheduledPaymentFrequency>
-        <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
-      </paymentPlan>
+      case DirectDebitSource.SA =>
+        <paymentPlan>
+          <actionType>01</actionType>
+          <pPType>02</pPType>
+          <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
+          <hodService>{hodService}</hodService>
+          <paymentCurrency>GBP</paymentCurrency>
+          <scheduledPaymentAmount>{request.regularPaymentAmount.getOrElse(BigDecimal(0))}</scheduledPaymentAmount>
+          <scheduledPaymentStartDate>{request.planStartDate.map(_.enteredDate).getOrElse("")}</scheduledPaymentStartDate>
+          <scheduledPaymentEndDate>{request.planEndDate.getOrElse("")}</scheduledPaymentEndDate>
+          <scheduledPaymentFrequency>05</scheduledPaymentFrequency>
+          <totalLiability>{request.totalAmountDue.getOrElse(BigDecimal(0))}</totalLiability>
+        </paymentPlan>
 
-    case _ =>
-      <paymentPlan>
-        <actionType>01</actionType>
-        <pPType>01</pPType>
-        <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
-        <hodService>{request.serviceType.toString}</hodService>
-        <paymentCurrency>GBP</paymentCurrency>
-      </paymentPlan>
-  }
+      case _ =>
+        <paymentPlan>
+          <actionType>01</actionType>
+          <pPType>01</pPType>
+          <paymentReference>{request.paymentReference.getOrElse("")}</paymentReference>
+          <hodService>{hodService}</hodService>
+          <paymentCurrency>GBP</paymentCurrency>
+        </paymentPlan>
+    }
 }
