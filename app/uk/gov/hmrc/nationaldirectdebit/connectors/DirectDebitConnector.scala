f5543f0e07989c2bb9bbe06fe6a9f31b86b48200
Sonal Gupta <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-10-16 18:23:46 +0100

    Dtr 376 (#24)
    * DTR-376: Connection calls for rds
    
    * DTR-376: changing the duplicate plan check url
    
    * DTR-376: Fixing URL and minor changes
    
    * DTR-376:Adding the test cases
    
    * DTR-376: Adding response for Duplicate Check
    
    * DTR:376: minor changes
    
    * DTR-376:Fixing minor issues
    
    * DTR-376: Adding the conflict change left behind
    
    * DTR-376: changing the frequency of duplicateRequest to Int
    
    * DTR-376: Adding the missed startDate
    
    * DTR-376: Adding the optional frequency

--- app/uk/gov/hmrc/nationaldirectdebit/connectors/DirectDebitConnector.scala
+++ app/uk/gov/hmrc/nationaldirectdebit/connectors/DirectDebitConnector.scala
@@ -20,8 +20,8 @@ import com.google.inject.Inject
 import play.api.libs.json.Json
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{HeaderCarrier, HttpReadsInstances, StringContextOps}
-import uk.gov.hmrc.nationaldirectdebit.models.responses.{EarliestPaymentDateResponse, GenerateDdiRefResponse, RDSDDPaymentPlansResponse, RDSDatacacheResponse, RDSPaymentPlanResponse}
-import uk.gov.hmrc.nationaldirectdebit.models.requests.{GenerateDdiRefRequest, WorkingDaysOffsetRequest}
+import uk.gov.hmrc.nationaldirectdebit.models.responses.*
+import uk.gov.hmrc.nationaldirectdebit.models.requests.*
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import play.api.libs.ws.JsonBodyWritables.writeableOf_JsValue
 
@@ -67,5 +67,12 @@ class DirectDebitConnector @Inject() (
     http
       .get(url"$rdsDatacacheProxyBaseUrl/direct-debits/$directDebitReference/payment-plans/$paymentPlanReference")(hc)
       .execute[RDSPaymentPlanResponse]
+  }
+
+  def isDuplicatePaymentPlan(request: PaymentPlanDuplicateCheckRequest)(implicit hc: HeaderCarrier): Future[DuplicateCheckResponse] = {
+    http
+      .post(url"$rdsDatacacheProxyBaseUrl/direct-debits/${request.directDebitReference}/duplicate-plan-check")(hc)
+      .withBody(Json.toJson(request))
+      .execute[DuplicateCheckResponse]
   }
 }
--- it/test/connectors/DirectDebitConnectorSpec.scala
+++ it/test/connectors/DirectDebitConnectorSpec.scala
@@ -16,7 +16,7 @@
 
 package connectors
 
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, post, stubFor, urlPathMatching}
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import itutil.ApplicationWithWiremock
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.must.Matchers
@@ -25,86 +25,116 @@ import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
 import play.api.libs.json.Json
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.nationaldirectdebit.connectors.DirectDebitConnector
-import uk.gov.hmrc.nationaldirectdebit.models.requests.{GenerateDdiRefRequest, WorkingDaysOffsetRequest}
-import uk.gov.hmrc.nationaldirectdebit.models.responses.{DirectDebitDetail, EarliestPaymentDateResponse, GenerateDdiRefResponse, PaymentPlanDetail, RDSDDPaymentPlansResponse, RDSDatacacheResponse, RDSDirectDebitDetails, RDSPaymentPlan, RDSPaymentPlanResponse}
+import uk.gov.hmrc.nationaldirectdebit.models.requests.{GenerateDdiRefRequest, PaymentPlanDuplicateCheckRequest, WorkingDaysOffsetRequest}
+import uk.gov.hmrc.nationaldirectdebit.models.responses.*
 
 import java.time.{LocalDate, LocalDateTime}
 
-class DirectDebitConnectorSpec extends ApplicationWithWiremock
-  with Matchers
-  with ScalaFutures
-  with IntegrationPatience {
+class DirectDebitConnectorSpec extends ApplicationWithWiremock with Matchers with ScalaFutures with IntegrationPatience {
 
   implicit val hc: HeaderCarrier = HeaderCarrier()
 
   val connector: DirectDebitConnector = app.injector.instanceOf[DirectDebitConnector]
 
   val testEmptyDataCacheResponse: RDSDatacacheResponse = RDSDatacacheResponse(directDebitCount = 0, directDebitList = Seq.empty)
-  val testDataCacheResponse: RDSDatacacheResponse = RDSDatacacheResponse(directDebitCount = 2,
+  val testDataCacheResponse: RDSDatacacheResponse = RDSDatacacheResponse(
+    directDebitCount = 2,
     directDebitList = Seq(
-      RDSDirectDebitDetails(ddiRefNumber = "testRef", submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12), bankSortCode = "testCode", bankAccountNumber = "testNumber", bankAccountName = "testName", auDdisFlag = true, numberOfPayPlans = 1),
-      RDSDirectDebitDetails(ddiRefNumber = "testRef", submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12), bankSortCode = "testCode", bankAccountNumber = "testNumber", bankAccountName = "testName", auDdisFlag = true, numberOfPayPlans = 1)
-    ))
+      RDSDirectDebitDetails(
+        ddiRefNumber       = "testRef",
+        submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12),
+        bankSortCode       = "testCode",
+        bankAccountNumber  = "testNumber",
+        bankAccountName    = "testName",
+        auDdisFlag         = true,
+        numberOfPayPlans   = 1
+      ),
+      RDSDirectDebitDetails(
+        ddiRefNumber       = "testRef",
+        submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12),
+        bankSortCode       = "testCode",
+        bankAccountNumber  = "testNumber",
+        bankAccountName    = "testName",
+        auDdisFlag         = true,
+        numberOfPayPlans   = 1
+      )
+    )
+  )
 
   val testDDPaymentPlansCacheResponse: RDSDDPaymentPlansResponse = RDSDDPaymentPlansResponse(
-    bankSortCode = "sort code",
+    bankSortCode      = "sort code",
     bankAccountNumber = "account number",
-    bankAccountName = "account name",
-    auDdisFlag = "dd",
-    paymentPlanCount = 2,
+    bankAccountName   = "account name",
+    auDdisFlag        = "dd",
+    paymentPlanCount  = 2,
     paymentPlanList = Seq(
       RDSPaymentPlan(
         scheduledPaymentAmount = 100,
-        planRefNumber = "ref number 1",
-        planType = "type 1",
-        paymentReference = "payment ref 1",
-        hodService = "service 1",
-        submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12)),
+        planRefNumber          = "ref number 1",
+        planType               = "type 1",
+        paymentReference       = "payment ref 1",
+        hodService             = "service 1",
+        submissionDateTime     = LocalDateTime.of(2025, 12, 12, 12, 12)
+      ),
       RDSPaymentPlan(
         scheduledPaymentAmount = 100,
-        planRefNumber = "ref number 1",
-        planType = "type 1",
-        paymentReference = "payment ref 1",
-        hodService = "service 1",
-        submissionDateTime = LocalDateTime.of(2025, 12, 12, 12, 12))
+        planRefNumber          = "ref number 1",
+        planType               = "type 1",
+        paymentReference       = "payment ref 1",
+        hodService             = "service 1",
+        submissionDateTime     = LocalDateTime.of(2025, 12, 12, 12, 12)
+      )
     )
   )
 
   val testEmptyDDPaymentPlansCacheResponse: RDSDDPaymentPlansResponse = RDSDDPaymentPlansResponse(
-    bankSortCode = "sort code",
+    bankSortCode      = "sort code",
     bankAccountNumber = "account number",
-    bankAccountName = "account name",
-    auDdisFlag = "dd",
-    paymentPlanCount = 2,
-    paymentPlanList = Seq.empty
+    bankAccountName   = "account name",
+    auDdisFlag        = "dd",
+    paymentPlanCount  = 2,
+    paymentPlanList   = Seq.empty
   )
 
   private val currentTime = LocalDateTime.MIN
 
   val testPaymentPlanResponse: RDSPaymentPlanResponse = RDSPaymentPlanResponse(
-    directDebitDetails = DirectDebitDetail(
-      bankSortCode = Some("sort code"),
-      bankAccountNumber = Some("account number"),
-      bankAccountName = None,
-      auDdisFlag = true,
-      submissionDateTime = currentTime),
+    directDebitDetails = DirectDebitDetail(bankSortCode = Some("sort code"),
+                                           bankAccountNumber  = Some("account number"),
+                                           bankAccountName    = None,
+                                           auDdisFlag         = true,
+                                           submissionDateTime = currentTime
+                                          ),
     paymentPlanDetails = PaymentPlanDetail(
-      hodService = "hod service",
-      planType = "plan Type",
-      paymentReference = "payment reference",
-      submissionDateTime = currentTime,
-      scheduledPaymentAmount = Some(1000),
+      hodService                = "hod service",
+      planType                  = "plan Type",
+      paymentReference          = "payment reference",
+      submissionDateTime        = currentTime,
+      scheduledPaymentAmount    = Some(1000),
       scheduledPaymentStartDate = Some(currentTime.toLocalDate),
-      initialPaymentStartDate = Some(currentTime.toLocalDate),
-      initialPaymentAmount = Some(150),
-      scheduledPaymentEndDate = Some(currentTime.toLocalDate),
+      initialPaymentStartDate   = Some(currentTime.toLocalDate),
+      initialPaymentAmount      = Some(150),
+      scheduledPaymentEndDate   = Some(currentTime.toLocalDate),
       scheduledPaymentFrequency = Some(1),
-      suspensionStartDate = Some(currentTime.toLocalDate),
-      suspensionEndDate = None,
-      balancingPaymentAmount = Some(600),
-      balancingPaymentDate = Some(currentTime.toLocalDate),
-      totalLiability = None,
-      paymentPlanEditable = false)
+      suspensionStartDate       = Some(currentTime.toLocalDate),
+      suspensionEndDate         = None,
+      balancingPaymentAmount    = Some(600),
+      balancingPaymentDate      = Some(currentTime.toLocalDate),
+      totalLiability            = None,
+      paymentPlanEditable       = false
+    )
+  )
+
+  val duplicateCheckRequest: PaymentPlanDuplicateCheckRequest = PaymentPlanDuplicateCheckRequest(
+    directDebitReference = "testRef",
+    paymentPlanReference = "payment ref 123",
+    planType             = "01",
+    paymentService       = "CESA",
+    paymentReference     = "payment ref",
+    paymentAmount        = 120.00,
+    totalLiability       = 780.00,
+    paymentFrequency     = Some(1),
+    paymentStartDate     = currentTime.toLocalDate
   )
 
   "DirectDebtConnector" should {
@@ -373,5 +403,67 @@ class DirectDebitConnectorSpec extends ApplicationWithWiremock
         result.getMessage must include("The future returned an exception")
       }
     }
+
+    "isDuplicatePaymentPlan" should {
+      "successfully retrieve true if is a duplicate payment plan" in {
+        stubFor(
+          post(urlPathMatching("/rds-datacache-proxy/direct-debits/testRef/duplicate-plan-check"))
+            .willReturn(
+              aResponse()
+                .withStatus(OK)
+                .withBody(Json.toJson(DuplicateCheckResponse(true)).toString)
+            )
+        )
+
+        val result: DuplicateCheckResponse = connector.isDuplicatePaymentPlan(duplicateCheckRequest).futureValue
+
+        result mustBe DuplicateCheckResponse(true)
+      }
+
+      "successfully retrieve false if it is not a duplicate payment plan" in {
+        stubFor(
+          post(urlPathMatching("/rds-datacache-proxy/direct-debits/testRef/duplicate-plan-check"))
+            .willReturn(
+              aResponse()
+                .withStatus(OK)
+                .withBody(Json.toJson(DuplicateCheckResponse(false)).toString)
+            )
+        )
+
+        val result: DuplicateCheckResponse = connector.isDuplicatePaymentPlan(duplicateCheckRequest).futureValue
+
+        result mustBe DuplicateCheckResponse(false)
+      }
+
+      "must fail when the result is parsed as an UpstreamErrorResponse" in {
+        stubFor(
+          post(urlPathMatching("/rds-datacache-proxy/direct-debits/testRef/duplicate-plan-check"))
+            .willReturn(
+              aResponse()
+                .withStatus(INTERNAL_SERVER_ERROR)
+                .withBody("test error")
+            )
+        )
+
+        val result = intercept[Exception](connector.isDuplicatePaymentPlan(duplicateCheckRequest).futureValue)
+
+        result.getMessage must include("returned 500. Response body: 'test error'")
+      }
+
+      "must fail when the result is a failed future" in {
+        stubFor(
+          get(urlPathMatching("/rds-datacache-proxy/direct-debits/testRef/duplicate-plan-check"))
+            .willReturn(
+              aResponse()
+                .withStatus(0)
+            )
+        )
+
+        val result = intercept[Exception](connector.isDuplicatePaymentPlan(duplicateCheckRequest).futureValue)
+
+        result.getMessage must include("The future returned an exception")
+      }
+    }
+
   }
 }
