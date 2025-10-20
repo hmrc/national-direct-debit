63e2a25e79093a096d986f6d58538ff1c255e937
sonalgupta1227 <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-10-20 14:48:39 +0100

    DTR-376: Fixing TotalLiability and payment amount to be optional

--- test/controllers/DirectDebitControllerSpec.scala
+++ test/controllers/DirectDebitControllerSpec.scala
@@ -324,8 +324,8 @@ class DirectDebitControllerSpec extends SpecBase {
       planType             = "01",
       paymentService       = "CESA",
       paymentReference     = "payment ref",
-      paymentAmount        = 120.00,
-      totalLiability       = 780.00,
+      paymentAmount        = Some(120.00),
+      totalLiability       = Some(120.00),
       paymentFrequency     = Some(1),
       paymentStartDate     = currentTime.toLocalDate
     )
